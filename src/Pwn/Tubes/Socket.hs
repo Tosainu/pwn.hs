8d539341bfb68e84a6983f6556b70c4285f9112f
Kenta Sato <tosainu.maple@gmail.com>
2017-08-21 18:37:03 +0900

    Use Handle

--- src/Pwn/Tubes/Socket.hs
+++ src/Pwn/Tubes/Socket.hs
@@ -3,18 +3,19 @@ module Pwn.Tubes.Socket
   , remote
   ) where
 
-import           Control.Monad.IO.Class    (liftIO)
-import           Data.ByteString.Char8     (ByteString)
-import           Data.Monoid               ((<>))
-import qualified Network.Socket            as NS hiding (recv)
-import qualified Network.Socket.ByteString as NS
+import           Control.Monad.IO.Class (liftIO)
+import           Data.ByteString.Char8  (ByteString)
+import qualified Data.ByteString.Char8  as BS
+import           Data.Monoid            ((<>))
+import qualified Network.Socket         as NS
+import           System.IO
 
 import           Pwn.Log
-import qualified Pwn.Tubes.Tube            as T
+import qualified Pwn.Tubes.Tube         as T
 
 data Socket = Socket { address :: String
                      , port    :: Int
-                     , socket  :: NS.Socket
+                     , hsocket :: Handle
                      }
 
 instance T.Tube Socket where
@@ -33,26 +34,28 @@ remote a p = do
   ai <- head <$> NS.getAddrInfo Nothing (Just a) (Just $ show p)
   sock <- NS.socket (NS.addrFamily ai) NS.Stream NS.defaultProtocol
   NS.connect sock (NS.addrAddress ai)
+  h <- NS.socketToHandle sock ReadWriteMode
+  hSetBuffering h NoBuffering
   liftIO $ success $ logstr <> ": Done"
-  return $ Socket a p sock
+  return $ Socket a p h
 
 recv :: Socket -> IO ByteString
-recv sock = recvn sock 4096
+recv sock = BS.hGetSome (hsocket sock) 4096
 
 recvn :: Socket -> Int -> IO ByteString
-recvn sock len = NS.recv (socket sock) len
+recvn sock len = BS.hGet (hsocket sock) len
 
 send :: Socket -> ByteString -> IO ()
-send sock str = NS.sendAll (socket sock) str
+send sock str = BS.hPut (hsocket sock) str
 
 wait :: Socket -> IO ()
 wait _ = error "not implemented yet"
 
 close :: Socket -> IO ()
-close = NS.close . socket
+close = hClose . hsocket
 
 shutdown :: Socket -> IO ()
-shutdown sock = NS.shutdown (socket sock) NS.ShutdownBoth
+shutdown = close
 
 interactive :: Socket -> IO ()
 interactive _ = error "not implemented yet"
