60fa11470302030732c50d60505e4fb1500c8b0a
Kenta Sato <tosainu.maple@gmail.com>
2017-09-18 11:49:03 +0900

    Add test for disasm

--- test/Pwn/AsmSpec.hs
+++ test/Pwn/AsmSpec.hs
@@ -5,7 +5,8 @@ module Pwn.AsmSpec
   , spec
   ) where
 
-import           Control.Monad.Reader (runReaderT)
+import           Control.Monad.Reader  (runReaderT)
+import qualified Data.ByteString.Char8 as BS
 import           Data.Either
 import           Pwn.Asm
 import           Pwn.Config
@@ -63,7 +64,23 @@ spec = do
 
   describe "Pwn.Asm.disasm" $ do
     it "disassemble i386 code" $ do
-      pending
+      let bin = "\x31\xdb\xb8\x01\x00\x00\x00\xcd\x80"
+      Right ret <- runReaderT (disasm bin) linux32Config
+      let retl = BS.lines ret
+      length retl `shouldBe` 4
+      "31 db"                `BS.isInfixOf` (retl !! 1) `shouldBe` True
+      "b8 01 00 00 00"       `BS.isInfixOf` (retl !! 2) `shouldBe` True
+      "cd 80"                `BS.isInfixOf` (retl !! 3) `shouldBe` True
 
     it "disassemble x86-64 code" $ do
-      pending
+      let bin = "\x48\x31\xff\x48\xc7\xc0\x3c\x00\x00\x00\x0f\x05"
+      Right ret <- runReaderT (disasm bin) linux64Config
+      let retl = BS.lines ret
+      length retl `shouldBe` 4
+      "48 31 ff"             `BS.isInfixOf` (retl !! 1) `shouldBe` True
+      "48 c7 c0 3c 00 00 00" `BS.isInfixOf` (retl !! 2) `shouldBe` True
+      "0f 05"                `BS.isInfixOf` (retl !! 3) `shouldBe` True
+
+    it "disassemble with invalid config" $ do
+      ret <- runReaderT (disasm "'x90") invalidConfig
+      (isLeft ret) `shouldBe` True
