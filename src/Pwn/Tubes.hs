d899fa0c573f0518a73067637ac51214cef5daf7
Kenta Sato <tosainu.maple@gmail.com>
2018-06-22 20:23:31 +0900

    Add enable-ssh-client flag to switch Pwn.Tubes.SSH

--- src/Pwn/Tubes.hs
+++ src/Pwn/Tubes.hs
@@ -1,8 +1,13 @@
+{-# LANGUAGE CPP #-}
+
 module Pwn.Tubes
   ( module PT
   ) where
 
 import           Pwn.Tubes.Process as PT
 import           Pwn.Tubes.Socket  as PT
-import           Pwn.Tubes.SSH     as PT
 import           Pwn.Tubes.Tube    as PT hiding (sink, source)
+
+#ifdef ENABLE_SSH_CLIENT
+import           Pwn.Tubes.SSH     as PT
+#endif
